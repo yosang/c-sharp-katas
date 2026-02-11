173e1e1a3dbd18dcd517caecd6e70580c3a89739
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-11 07:53:02 +0100

    solve welcome

---
+++ 8kyu/0.welcome/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+public static class Kata
+{
+
+    public static void Main()
+    {
+        Console.WriteLine(Greet("norsk")); // Welcome, norsk is not in the database
+        Console.WriteLine(Greet("finnish")); // Tervetuloa
+    }
+    public static string Greet(string language)
+    {
+        // We can use a Dictionary for key:value lookups
+        Dictionary<string, string> database = new Dictionary<string, string>()
+        {
+                {"english", "Welcome"}
+            , {"czech", "Vitejte"}
+            , {"danish", "Velkomst"}
+            , {"dutch", "Welkom"}
+            , {"estonian", "Tere tulemast"}
+            , {"finnish", "Tervetuloa"}
+            , {"flemish", "Welgekomen"}
+            , {"french", "Bienvenue"}
+            , {"german", "Willkommen"}
+            , {"irish", "Failte"}
+            , {"italian", "Benvenuto"}
+            , {"latvian", "Gaidits"}
+            , {"lithuanian", "Laukiamas"}
+            , {"polish", "Witamy"}
+            , {"spanish", "Bienvenido"}
+            , {"swedish", "Valkommen"}
+            , {"welsh", "Croeso"}
+        };
+
+        string greeting;
+
+        // We check if the passed language is in our database with .ContainsKey()
+        // If it does, return the greeting, if it doesnt default to english
+        greeting = database.ContainsKey(language) ? database[language] : database["english"];
+
+        return greeting;
+    }
+
+
+}
---
+++ 8kyu/0.welcome/readme.md
@@ -0,0 +1,39 @@
+https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+# Task
+Think of a way to store the languages as a database. 
+
+The languages are listed below so you can copy and paste!
+
+Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. 
+
+It should default to English if the language is not in the database, or in the event of an invalid input.
+
+# The database
+```c#
+[ ("english", "Welcome")
+, ("czech", "Vitejte")
+, ("danish", "Velkomst")
+, ("dutch", "Welkom")
+, ("estonian", "Tere tulemast")
+, ("finnish", "Tervetuloa")
+, ("flemish", "Welgekomen")
+, ("french", "Bienvenue")
+, ("german", "Willkommen")
+, ("irish", "Failte")
+, ("italian", "Benvenuto")
+, ("latvian", "Gaidits")
+, ("lithuanian", "Laukiamas")
+, ("polish", "Witamy")
+, ("spanish", "Bienvenido")
+, ("swedish", "Valkommen")
+, ("welsh", "Croeso")
+]
+```
+
+Possible invalid inputs include:
+```
+IP_ADDRESS_INVALID - not a valid ipv4 or ipv6 ip address
+IP_ADDRESS_NOT_FOUND - ip address not in the database
+IP_ADDRESS_REQUIRED - no ip address was supplied
+```
