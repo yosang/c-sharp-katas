0d524548cce766ad492db294b1057c41760af4d2
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-11 08:31:34 +0100

    solve 2.square-num

---
+++ 8kyu/2.square-sum/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(SquareSum(new int[] { 1, 2, 2 }));
+    }
+    public static int SquareSum(int[] numbers)
+    {
+        // Initialize sum as 0
+        int sum = 0;
+
+        foreach (int num in numbers)
+        {
+            // Square a number by multiplying it by itself.
+            sum += num * num;
+        }
+
+        return sum;
+    }
+}
