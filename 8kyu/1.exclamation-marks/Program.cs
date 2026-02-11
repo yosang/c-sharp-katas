559c6bd9891604920b586a8a6a421ab7c25150bc
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-11 08:06:05 +0100

    solve exclamation marks

---
+++ 8kyu/1.exclamation-marks/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(Remove("Hi!"));
+        Console.WriteLine(Remove("Hi!!!"));
+        Console.WriteLine(Remove("!Hi"));
+        Console.WriteLine(Remove("!Hi!"));
+        Console.WriteLine(Remove("Hi! Hi!"));
+        Console.WriteLine(Remove("Hi"));
+    }
+    public static string Remove(string s)
+    {
+        string parsed = $"{s.Replace("!", "")}!"; // interpolation
+        // string parsed = s.Replace("!", "") + "!"; // concatenation
+
+        return parsed;
+    }
+}
