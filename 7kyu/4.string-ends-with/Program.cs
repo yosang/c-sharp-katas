091ed5a835e079471f80ee2511166aa1472a316f
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-12 19:06:54 +0100

    solve string ends with

---
+++ 7kyu/4.string-ends-with/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+// WriteLine(Solution("abc", "bc"));
+// WriteLine(Solution("abc", "d"));
+
+WriteLine(SolutionL("abc", "bc"));
+WriteLine(SolutionL("abc", "d"));
+
+bool Solution(string str, string ending)
+{
+    return str.EndsWith(ending);
+}
+
+// lambda operator
+
+bool SolutionL(string str, string ending) => str.EndsWith(ending);
---
+++ 7kyu/4.string-ends-with/readme.md
@@ -0,0 +1,13 @@
+kata link: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+```
+Inputs: "abc", "bc"
+Output: true
+
+Inputs: "abc", "d"
+Output: false
+```
