649d8872cef1c2bd72099512e4a0468ce64245a8
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-20 10:46:55 +0100

    solve backspaces in a string

--- 6kyu/0.backspaces-in-a-string/Program.cs
+++ 6kyu/0.backspaces-in-a-string/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Program
+﻿using System.Collections.Generic;
+
+namespace Program
 {
     public class App
     {
+        public class TextCannotBeEmpty : Exception
+        {
+            public TextCannotBeEmpty(string message) : base(message) { }
+        }
+
+        public static string ProcessBackspaces(string text, char symbol = '#')
+        {
+            // No point in processing an empty string, so just throw an exception if its empty before moving on
+            if (text.Length < 1) throw new TextCannotBeEmpty("Text cannot be empty");
+
+            Stack<char> myStack = new Stack<char>(); // We are using a stack for the backspacing = everytime we encounter a symbol, we pop from the stack, otherwise push
+
+            // Iterate each character of the text string, if its not a #, push it to our stack
+            // Otherwise check if we have items in our stack, and remove pop them each time we encounter a #
+            foreach (char c in text)
+            {
+                if (c != symbol)
+                {
+                    myStack.Push(c);
+                }
+                else
+                {
+                    if (myStack.Count > 0) myStack.Pop(); // Make sure we pop only if there are items in the stack
+                }
+            }
+
+            // We can reverse a stack by creating a new stack from the original stack
+
+            return string.Join("", new Stack<char>(myStack));
+        }
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            string test1 = ProcessBackspaces("abc#d##c");
+            string test2 = ProcessBackspaces("abc##d######"); // should return and empty string
+            string test3 = ProcessBackspaces("#######");
+            // string test4 = ProcessBackspaces(""); // throws an exception
+
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+            Console.WriteLine(test3);
+            // Console.WriteLine(test4);
         }
     }
 }
--- 6kyu/0.backspaces-in-a-string/readme.md
+++ 6kyu/0.backspaces-in-a-string/readme.md
@@ -1,5 +1,7 @@
+# Source
 Kata link: https://www.codewars.com/kata/5727bb0fe81185ae62000ae3
 
+# Instructions
 Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
 
 Your task is to process a string with "#" symbols.
