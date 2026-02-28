5d6c7c0acafb7a603b9630b81b860d752c645369
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-28 10:56:34 +0100

    solve: count-characters

--- 6kyu/3.count-characters/Program.cs
+++ 6kyu/3.count-characters/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+public class Kata
+{
+    public static void PrintADictionary(Dictionary<char, int> dict)
+    {
+        // Debugging: Loop the dictionary to see the output
+        foreach (KeyValuePair<char, int> item in dict)
+        {
+            Console.WriteLine($"\"{item.Key}\":\"{item.Value}\"");
+        }
+    }
+    public static Dictionary<char, int> Count(string str)
+    {
+        Dictionary<char, int> myDict = new();
+
+        foreach (char c in str)
+        {
+            if (!myDict.ContainsKey(c))
+            {
+                myDict[c] = 1;
+            }
+            else
+            {
+                myDict[c]++;
+            }
+        }
+
+        return myDict;
+    }
+    public static void Main()
+    {
+        // PrintADictionary(Count("abc"));
+        // PrintADictionary(Count("abcc"));
+        PrintADictionary(Count("abbcc"));
+    }
+}
