36bd18f6704c6f4a17f82ae35d24290c7a120453
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-28 21:25:29 +0100

    chore: implement partial solution

--- 6kyu/4.consecutive-strings/Program.cs
+++ 6kyu/4.consecutive-strings/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+public class Kata
+{
+    public static string longest_consec(string[] arr)
+    {
+        List<string> result = new();
+
+        int i = 0;
+        while (i < arr.Length - 1)
+        {
+            StringBuilder sb = new();
+            sb.Append(string.Concat(arr[i], arr[i + 1]));
+            result.Add(sb.ToString());
+            i++;
+        }
+
+        List<string> sorted = result.OrderByDescending(x => x.Length).ToList();
+
+        return sorted[0];
+    }
+
+    public static void Main()
+    {
+        // string[] test1 = new string[] { "tree", "foling", "trashy", "blue", "abcdef", "uvwxyz" }; 
+        string[] test1 = new string[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" };
+
+        Console.WriteLine(longest_consec(test1));
+    }
+}
